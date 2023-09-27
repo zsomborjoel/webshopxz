18f783521368d671df95b00bba7af9153ffd1e04
Zsombor Joel Gyurkovics <zsomborjoel@gmail.com>
2023-09-27 20:30:37 +0200

    Make main page more responsive

--- internal/webpage/template/loginpage/routers.go
+++ internal/webpage/template/loginpage/routers.go
@@ -7,8 +7,13 @@ import (
 
 func LoginPageRegister(r *gin.RouterGroup) {
 	r.GET("/login", renderLoginPage)
+	r.GET("/create-account", renderCreateAccount)
 }
 
 func renderLoginPage(c *gin.Context) {
 	common.GetTemplate().ExecuteTemplate(c.Writer, "indexHTMLloginpage", nil)
+}
+
+func renderCreateAccount(c *gin.Context) {
+	common.GetTemplate().ExecuteTemplate(c.Writer, "createaccountHTMLloginpage", nil)
 }

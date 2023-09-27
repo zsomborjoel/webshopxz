3d97f597be8f799e41653b3b511d4f6132884a0a
Zsombor Joel Gyurkovics <zsomborjoel@gmail.com>
2023-09-27 10:50:09 +0200

    Common structure for folders

--- internal/webpage/template/loginpage/routers.go
+++ internal/webpage/template/loginpage/routers.go
@@ -7,13 +7,8 @@ import (
 
 func LoginPageRegister(r *gin.RouterGroup) {
 	r.GET("/login", renderLoginPage)
-	r.GET("/test", test)
 }
 
 func renderLoginPage(c *gin.Context) {
 	common.GetTemplate().ExecuteTemplate(c.Writer, "indexHTMLloginpage", nil)
-}
-
-func test(c *gin.Context) {
-	common.GetTemplate().ExecuteTemplate(c.Writer, "test", nil)
 }

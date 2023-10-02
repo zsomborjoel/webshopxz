0e92a53d85d060db49e352f4172438c0fabdba74
Zsombor Joel Gyurkovics <zsomborjoel@gmail.com>
2023-10-02 15:55:34 +0200

    Session config options added

--- internal/auth/session/store.go
+++ internal/auth/session/store.go
@@ -1,8 +1,11 @@
 package session
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/rs/zerolog/log"
 )
@@ -13,5 +16,40 @@ func SetupStore() cookie.Store {
 		log.Error().Msg("SESSION_STORE_KEY environment variable is not set")
 	}
 
-	return cookie.NewStore([]byte(storekey))
+	maxAgeValue := os.Getenv("SESSION_STORE_MAX_AGE")
+	if maxAgeValue == "" {
+		maxAgeValue = "86400" // 1 day in seconds
+		log.Error().Msg("SESSION_STORE_MAX_AGE environment variable is not set")
+	}
+
+	maxAge, err := strconv.Atoi(maxAgeValue)
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("SESSION_STORE_MAX_AGE can not be converted to int: %s", maxAgeValue))
+	}
+
+	secureValue := os.Getenv("SESSION_STORE_SECURE")
+	if secureValue == "" {
+		secureValue = "false" // Set to true if using HTTPS
+		log.Error().Msg("SESSION_STORE_SECURE environment variable is not set")
+	}
+
+	secure, err := strconv.ParseBool(secureValue)
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("SESSION_STORE_SECURE can not be converted to bool: %s", secureValue))
+	}
+
+	domain := os.Getenv("SESSION_STORE_DOMAIN")
+	if domain == "" {
+		log.Error().Msg("SESSION_STORE_DOMAIN environment variable is not set")
+	}
+
+	store := cookie.NewStore([]byte(storekey))
+	store.Options(sessions.Options{
+		MaxAge:   maxAge,
+		HttpOnly: true, // Against XSS by fetching document.cookie with JS
+		Secure:   secure,
+		Domain:   domain,
+	})
+
+	return store
 }
